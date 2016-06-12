cf6b79046c7f321cbccda90b4f77bff83247325b
mateosss <mateodemayo@gmail.com>
2016-06-12 16:20:37 -0300

    Map scroll limits working

--- src/game.js
+++ src/game.js
@@ -77,15 +77,17 @@ var Level = cc.Layer.extend({
     //   },
     // }, this);
 
-    // TODO# Scroll limit
-    // TODO# ZOOM NOT WORKING WITH TOUCHSCREEN
     // TODO Better zoom (zoom where the mouse is or where the touch is made)
+    // TODO# ZOOM NOT WORKING WITH TOUCHSCREEN
     // TODO Better zoom with lerp
     // TODO Add parallax background to the map
+    // TODO pasar update y movemap a map
     if ('touches' in cc.sys.capabilities){
       cc.eventManager.addListener({
         event: cc.EventListener.TOUCH_ALL_AT_ONCE,
         onTouchesMoved: function (touches, event) {
+          // TODO hacer padre que tenga moveButton, zoomButton map y herede aca
+          // TODO Update pasarlo al map, por que update cosas suyas
           var node = event.getCurrentTarget().getChildByTag(TAG_TILE_MAP);
           var touch = touches[0];
           var delta = touch.getDelta();
@@ -96,36 +98,84 @@ var Level = cc.Layer.extend({
     } else if ('mouse' in cc.sys.capabilities)
     cc.eventManager.addListener({
       event: cc.EventListener.MOUSE,
+      moveButton: cc.EventMouse.BUTTON_LEFT,
+      zoomButton: cc.EventMouse.BUTTON_RIGHT,
+      map: null,
+      moveMap: function(x, y) {
+        mapHalfWidth = (this.map.width * this.map.scale)/2;
+        mapHalfHeight = (this.map.height)/2;
+
+        maxLeft = winSize.width - 50 - mapHalfWidth;
+        maxRight = 0 + 50 + mapHalfWidth;
+        maxDown = winSize.height + 50 - mapHalfHeight;
+        maxUp = 0 - 50 + mapHalfHeight;
+
+        newX = this.map.positionTarget.x + x;
+        newY = this.map.positionTarget.y + y;
+
+        if (newX < maxLeft) {
+          newX = maxLeft;
+        }
+        if (newX > maxRight) {
+          newX = maxRight;
+        }
+
+        if (newY < maxDown) {
+          newY = maxDown;
+        }
+        if (newY > maxUp) {
+          newY = maxUp;
+        }
+        this.map.positionTarget.x = newX;
+        this.map.positionTarget.y = newY;
+
+        // if (this.map.x > maxBorderLeft){
+        //     this.map.positionTarget.x = maxBorderLeft;
+        // } else {
+        //   this.map.positionTarget += x;
+        // }
+
+        // if (targetX > -(this.map.width * this.map.scale)/2 + winSize.width - 50 &&
+        // targetX < (this.map.width * this.map.scale)/2 + 50) {
+        //
+        // }
+        // if (targetY > -(this.map.height * this.map.scale)/2 + winSize.height - 50 &&
+        // targetY < (this.map.height * this.map.scale)/2 + 50) {
+        //   // this.map.positionTarget.y += y;
+        // }
+        // this.map.positionTarget.x = targetX;
+        // this.map.positionTarget.y = targetY;
+      },
       onMouseDown: function(event){
         this.pressed = event.getButton();
+        this.clickLocation = event.getLocation();
+
       },
       onMouseUp: function(event){
         this.pressed = -1;
       },
       onMouseMove: function(event){
         if (this.pressed != -1) {
-          var node = event.getCurrentTarget().getChildByTag(TAG_TILE_MAP);
-          if(this.pressed == cc.EventMouse.BUTTON_LEFT){
-            var delta = event.getDelta();
-            targetX = node.positionTarget.x + delta.x;
-            targetY = node.positionTarget.y + delta.y;
-            // if (targetX > -(node.width * node.scale)/2 + winSize.width - 50 &&
-            //     targetX < (node.width * node.scale)/2 + 50) {
-            //   node.positionTarget.x = targetX;
-            // }
-            // if (targetY > -(node.height * node.scale)/2 + winSize.height - 50 &&
-            //     targetY < (node.height * node.scale)/2 + 50) {
-            //   node.positionTarget.y = targetY;
-            // }
-            node.positionTarget.y = targetY;
-            node.positionTarget.x = targetX;
-
+          this.map = event.getCurrentTarget().getChildByTag(TAG_TILE_MAP);
+          if(this.pressed == this.moveButton){
+            delta = event.getDelta();
+            // targetX = this.map.positionTarget.x + delta.x;
+            // targetY = this.map.positionTarget.y + delta.y;
+            this.moveMap(delta.x, delta.y);
           }
-          else if(this.pressed == cc.EventMouse.BUTTON_RIGHT){
+          else if(this.pressed == this.zoomButton){
             zoomDelta = event.getDeltaY()*0.001;
-            zoom = node.scale + zoomDelta;
+            zoom = this.map.scale + zoomDelta;
             if (zoom >= 0.15 && zoom <= 1.0) {
-              node.scale = zoom;
+              this.map.scale = zoom;
+              mapCenter = this.map.convertToWorldSpaceAR(this.map.getAnchorPoint());
+              difference = cc.pSub(mapCenter, this.clickLocation);
+              // TODO para que funcione el zoom hacia algo
+              // obtener la posicion en el mapa de mi click
+              // setear esa posicion como el anchor point
+              // escalar desde ahi
+              // dejar el anchor point donde estaba
+              // this.moveMap(difference.x * zoomDelta * 10, 0);
             }
           }
         }
