b5b3681825ee134d9a28200c90acb3bb60ba3ff6
mateosss <mateodemayo@gmail.com>
2017-12-17 21:40:06 -0300

    predefinedValues for progress component

--- src/hud/components/progress.js
+++ src/hud/components/progress.js
@@ -7,12 +7,15 @@ var Progress = Panel.extend({
   bar: null,
   text: null,
 
+  selectedValue: 0, // If using predefinedValues this will have the index of the current one
+
   ctor: function(options) {
     this.progress = this.progress || {
       text: options.text || "{}%", // text to apply _.format(text, percent)
       percentage: options.percentage || 0,
       fontName: options.fontName || r.getDefaultFont(),
       fontSize: options.text || 32,
+      predefinedValues: options.predefinedValues || null // array with predefined values to set with setValue
     };
     options.bgImage = options.bgImage || r.ui.panel_in_soft;
     this._super(options);
@@ -26,12 +29,26 @@ var Progress = Panel.extend({
   setup: function(options) {
     this.progress.percentage = options.percentage !== undefined ? options.percentage : this.progress.percentage;
     this.progress.text = options.text !== undefined ? options.text : this.progress.text;
+    this.progress.predefinedValues = options.predefinedValues || this.progress.predefinedValues;
     if (this.bar) this.bar.setup({width: cc.lerp(4, 100, this.progress.percentage / 100) + "pw"});
-    if (this.text) this.text.setup({text: _.format(this.progress.text, this.progress.percentage)});
+    if (this.text && !this.progress.predefinedValues) this.text.setup({text: _.format(this.progress.text, this.progress.percentage)});
     this._super(options);
   },
   setPercent: function(value) {
-    this.bar.setup({width: cc.lerp(4, 100, value / 100) + "pw"});
+    this.progress.percentage = value;
+    this.setup({});
+  },
+  setValue: function(i) {
+    this.selectedValue = i;
+    let values = this.progress.predefinedValues;
+    this.setPercent((i + 1) * 100 / values.length);
+    this.text.setup({text: _.format(this.progress.text, values[i])});
+  },
+  nextValue: function() {
+    if (this.selectedValue + 1 < this.progress.predefinedValues.length) this.setValue(this.selectedValue + 1);
+  },
+  previousValue: function() {
+    if (this.selectedValue - 1 >= 0) this.setValue(this.selectedValue - 1);
   },
   toString: () => "Dialog"
 });
