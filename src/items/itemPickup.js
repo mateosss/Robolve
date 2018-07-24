78d384049148780c8c6609e25e5e9ad569efdae3
mateosss <mateodemayo@gmail.com>
2018-07-23 23:16:43 -0300

    Disappear itemdrop after 10 secons

--- src/items/itemPickup.js
+++ src/items/itemPickup.js
@@ -17,7 +17,6 @@ var ItemPickup = cc.Sprite.extend({
   },
 
   drop: function() {
-    // TODO animate a drop from the robot
     const DROP_HEIGHT = 128;
     const DROP_RADIUS = 64; // drop at X between targetPosition.x +- DROP_RADIUS
     let targetPosition = cc.pAdd(this.getPosition(), cc.p((2 * Math.random() - 1) * DROP_RADIUS, 0));
@@ -30,6 +29,13 @@ var ItemPickup = cc.Sprite.extend({
     let drop = new cc.EaseBounceOut(new cc.MoveTo(0.2, targetPosition), 3);
     let actArray = [spawn, drop];
     this.runAction(new cc.Sequence(actArray));
+
+    // Disappear after some secons
+    this.scheduleOnce(() => {
+      let shrink = new cc.EaseBackIn(new cc.ScaleTo(0.5, 0));
+      let remove = new cc.RemoveSelf();
+      this.runAction(new cc.Sequence([shrink, remove]));
+    }, 10); // TODO time hardcoded
   },
 
   pickup: function() {
